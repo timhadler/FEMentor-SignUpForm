004629a974b03de9a9efc80df0b29672f0e364af
Tim <timhadler93@gmail.com>
2023-11-13 19:28:28 +1300

    Form data processed and error messaged rendered

--- app/js/script.js
+++ app/js/script.js
@@ -3,5 +3,29 @@ $(document).ready(function() {
 });
 
 function submitForm() {
-    console.log("Pressed!")
+    const data = $('#form').serialize();
+    const params = new URLSearchParams(data);
+
+    const fName = params.get('firstName');
+    const lName = params.get('lastName');
+    const email = params.get('email');
+    const password = params.get('password');
+
+    // Process
+    if (fName.length == 0) {
+        $('#fNameInput').addClass("errorDisplay");
+        $('#firstNameErr').css("display", "inline-block");
+    }
+    if (lName.length == 0) {
+        $('#lNameInput').addClass("errorDisplay");
+        $('#lastNameErr').css("display", "inline-block");
+    }
+    if (email.slice(-10) != "@gmail.com" || email.length < 11) {
+        $('#emailInput').addClass("errorDisplay");
+        $('#emailErr').css("display", "inline-block");
+    }
+    if (password.length == 0) {
+        $('#passwordInput').addClass("errorDisplay");
+        $('#passwordErr').css("display", "inline-block");
+    }
 }
